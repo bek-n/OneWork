e269886219dea137073d79b54e08cf88f0ea8937
Bek <nematjanov95@gmail.com>
2023-02-06 23:08:45 +0500

    first commit

---
+++ lib/view/components/pages/auth/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0E9D57),
+      body: Center(child: Text('OneWork',style:  ,)),
+    );
+  }
+}
