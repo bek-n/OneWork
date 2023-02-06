4b0602a12b0bc048725faacc4af4153b7d896ec8
Bek <nematjanov95@gmail.com>
2023-02-07 01:07:02 +0500

    fixing

--- lib/view/components/pages/auth/onboarding_page.dart
+++ lib/view/components/pages/auth/onboarding_page.dart
@@ -1,60 +1,198 @@
+import 'package:concentric_transition/concentric_transition.dart';
 import 'package:flutter/material.dart';
-import 'package:intro_screen_onboarding_flutter/introduction.dart';
-import 'package:intro_screen_onboarding_flutter/introscreenonboarding.dart';
-import 'package:one_work/view/components/pages/style/style.dart';
 
-class OnBoarding extends StatefulWidget {
-  const OnBoarding({super.key});
+final pages = [
+  const PageData(
+    icon: Icons.bubble_chart,
+    title: "Local news\nstories",
+    bgColor: Color(0xFF0043D0),
+    textColor: Colors.white,
+  ),
+  const PageData(
+    icon: Icons.format_size,
+    title: "Choose your\ninterests",
+    textColor: Colors.white,
+    bgColor: Color(0xFFFDBFDD),
+  ),
+  const PageData(
+    icon: Icons.hdr_weak,
+    title: "Drag and\ndrop to move",
+    bgColor: Color(0xFFFFFFFF),
+  ),
+];
 
-  @override
-  State<OnBoarding> createState() => _OnBoardingState();
-}
-
-class _OnBoardingState extends State<OnBoarding> {
-  final List<Introduction> list = [
-    Introduction(
-      title: 'Search your job',
-      subTitle:
-          'Figure out your top five priorities whether it is company culture, salary.',
-      imageUrl: 'assets/images/1.png',
-    ),
-    Introduction(
-      title: 'Browse jobs list',
-      subTitle:
-          'Our job list include several  industries, so you can find the best job for you.',
-      imageUrl: 'assets/images/2.png',
-    ),
-    Introduction(
-      title: 'Apply to best jobs',
-      subTitle:
-          'You can apply to your desirable jobs very quickly and easily with ease.',
-      imageUrl: 'assets/images/3.png',
-    ),
-    Introduction(
-      title: 'Make your career',
-      subTitle:
-          'We help you find your dream job based on your skillset, location, demand.',
-      imageUrl: 'assets/images/4.png',
-    ),
-  ];
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: IntroScreenOnboarding(
-      backgroudColor: Theme.of(context).scaffoldBackgroundColor,
-      introductionList: list,
-      onTapSkipButton: () {},
-    ));
+      body: ConcentricPageView(
+        colors: pages.map((p) => p.bgColor).toList(),
+        radius: screenWidth * 0.1,
+        // curve: Curves.ease,
+        nextButtonBuilder: (context) => Padding(
+          padding: const EdgeInsets.only(left: 3), // visual center
+          child: Icon(
+            Icons.navigate_next,
+            size: screenWidth * 0.08,
+          ),
+        ),
+        // itemCount: pages.length,
+        // duration: const Duration(milliseconds: 1500),
+        // opacityFactor: 2.0,
+        // scaleFactor: 0.2,
+        // verticalPosition: 0.7,
+        // direction: Axis.vertical,
+        // itemCount: pages.length,
+        // physics: NeverScrollableScrollPhysics(),
+        itemBuilder: (index) {
+          final page = pages[index % pages.length];
+          return SafeArea(
+            child: _Page(page: page),
+          );
+        },
+      ),
+    );
   }
 }
 
+class PageData {
+  final String? title;
+  final IconData? icon;
+  final Color bgColor;
+  final Color textColor;
 
+  const PageData({
+    this.title,
+    this.icon,
+    this.bgColor = Colors.white,
+    this.textColor = Colors.black,
+  });
+}
 
+class _Page extends StatelessWidget {
+  final PageData page;
 
+  const _Page({Key? key, required this.page}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    space(double p) => SizedBox(height: screenHeight * p / 100);
+    return Column(
+      children: [
+        space(10),
+        _Image(
+          page: page,
+          size: 190,
+          iconSize: 170,
+        ),
+        space(8),
+        _Text(
+          page: page,
+          style: TextStyle(
+            fontSize: screenHeight * 0.046,
+          ),
+        ),
+      ],
+    );
+  }
+}
 
-  // Image.asset('assets/images/5.png', width: 375, height: 460),
-  //         Image.asset('assets/images/6.png', width: 375, height: 460),
-  //         Image.asset('assets/images/7.png', width: 375, height: 460),
-  //         Image.asset('assets/images/8.png', width: 375, height: 460),
+class _Text extends StatelessWidget {
+  const _Text({
+    Key? key,
+    required this.page,
+    this.style,
+  }) : super(key: key);
+
+  final PageData page;
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      page.title ?? '',
+      style: TextStyle(
+        color: page.textColor,
+        fontWeight: FontWeight.w600,
+        fontFamily: 'Helvetica',
+        letterSpacing: 0.0,
+        fontSize: 18,
+        height: 1.2,
+      ).merge(style),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image({
+    Key? key,
+    required this.page,
+    required this.size,
+    required this.iconSize,
+  }) : super(key: key);
+
+  final PageData page;
+  final double size;
+  final double iconSize;
+
+  @override
+  Widget build(BuildContext context) {
+    final bgColor = page.bgColor
+        // .withBlue(page.bgColor.blue - 40)
+        .withGreen(page.bgColor.green + 20)
+        .withRed(page.bgColor.red - 100)
+        .withAlpha(90);
+
+    final icon1Color =
+        page.bgColor.withBlue(page.bgColor.blue - 10).withGreen(220);
+    final icon2Color = page.bgColor.withGreen(66).withRed(77);
+    final icon3Color = page.bgColor.withRed(111).withGreen(220);
+
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(60.0)),
+        color: bgColor,
+      ),
+      child: Stack(
+        clipBehavior: Clip.none,
+        fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: Icon(
+                page.icon,
+                size: iconSize + 20,
+                color: icon1Color,
+              ),
+            ),
+            right: -5,
+            bottom: -5,
+          ),
+          Positioned.fill(
+            child: RotatedBox(
+              quarterTurns: 5,
+              child: Icon(
+                page.icon,
+                size: iconSize + 20,
+                color: icon2Color,
+              ),
+            ),
+          ),
+          Icon(
+            page.icon,
+            size: iconSize,
+            color: icon3Color,
+          ),
+        ],
+      ),
+    );
+  }
+}
