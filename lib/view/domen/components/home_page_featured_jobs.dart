c657f5b0b9f31899f555a2baa0a08155ee67a8b7
Bek <nematjanov95@gmail.com>
2023-02-09 16:33:21 +0500

    added featured job to component

---
+++ lib/view/domen/components/home_page_featured_jobs.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../style/style.dart';
+
+class FeaturedJobs extends StatefulWidget {
+  final int index;
+  final String imageLogoCompany;
+  final String textPosition;
+  final String CompanyName;
+  final String wrap1;
+  final String wrap2;
+  final String wrap3;
+  final String salary;
+  final String textlocation;
+
+  const FeaturedJobs(
+      {super.key,
+      required this.index,
+      required this.imageLogoCompany,
+      required this.CompanyName,
+      required this.textPosition, required this.wrap1, required this.wrap2, required this.wrap3, required this.salary, required this.textlocation});
+
+  @override
+  State<FeaturedJobs> createState() => _FeaturedJobsState();
+}
+
+class _FeaturedJobsState extends State<FeaturedJobs> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 280.w,
+        height: 186.h,
+        margin: const EdgeInsets.only(right: 24),
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+              image: AssetImage('assets/images/Group.png'), fit: BoxFit.cover),
+          borderRadius: BorderRadius.circular(24),
+          color: Colors.primaries[widget.index % Colors.primaries.length],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  24.horizontalSpace,
+                  Container(
+                    height: 46.h,
+                    width: 46.w,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(widget.imageLogoCompany),
+                        ),
+                        color: Style.whiteColor,
+                        borderRadius: BorderRadius.circular(12)),
+                  ),
+                  16.horizontalSpace,
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.textPosition,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: Style.textStyleRegular2(
+                                textColor: Style.whiteColor),
+                          ),
+                          Text(
+                            widget.CompanyName,
+                            style: Style.textStyleRegular2(
+                                size: 14, textColor: Style.whiteColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 12, top: 24, right: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Wrap(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 4),
+                          decoration: BoxDecoration(
+                              color: Style.whiteColor.withOpacity(0.15),
+                              borderRadius: BorderRadius.circular(65)),
+                          child: Text(
+                            widget.wrap1,
+                            style: Style.textStyleRegular2(
+                                size: 12, textColor: Style.whiteColor),
+                          ),
+                        )
+                      ],
+                    ),
+                    Wrap(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 4),
+                          decoration: BoxDecoration(
+                              color: Style.whiteColor.withOpacity(0.15),
+                              borderRadius: BorderRadius.circular(65)),
+                          child: Text(
+                            widget.wrap2,
+                            style: Style.textStyleRegular2(
+                                size: 12, textColor: Style.whiteColor),
+                          ),
+                        )
+                      ],
+                    ),
+                    Wrap(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 4),
+                          decoration: BoxDecoration(
+                              color: Style.whiteColor.withOpacity(0.15),
+                              borderRadius: BorderRadius.circular(65)),
+                          child: Text(
+                             widget.wrap3,
+                            style: Style.textStyleRegular2(
+                                size: 12, textColor: Style.whiteColor),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              24.verticalSpace,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.salary,
+                      style: Style.textStyleRegular2(
+                          size: 13, textColor: Style.whiteColor),
+                    ),
+                    Text(
+                      widget.textlocation,
+                      style: Style.textStyleRegular2(
+                          size: 13, textColor: Style.whiteColor),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+    ;
+  }
+}
--- lib/view/pages/home/home_page.dart
+++ lib/view/pages/home/home_page.dart
@@ -4,6 +4,8 @@ import 'package:icons_plus/icons_plus.dart';
 import 'package:one_work/view/domen/components/custom_textfromfiled.dart';
 import 'package:one_work/view/style/style.dart';
 
+import '../../domen/components/home_page_featured_jobs.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -111,158 +113,18 @@ class _HomePageState extends State<HomePage> {
                     scrollDirection: Axis.horizontal,
                     itemCount: 5,
                     itemBuilder: (context, index) {
-                      return Container(
-                          width: 280.w,
-                          height: 186.h,
-                          margin: const EdgeInsets.only(right: 24),
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage('assets/images/Group.png'),
-                                fit: BoxFit.cover),
-                            borderRadius: BorderRadius.circular(24),
-                            color: Colors
-                                .primaries[index % Colors.primaries.length],
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(top: 24),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    24.horizontalSpace,
-                                    Container(
-                                      height: 46.h,
-                                      width: 46.w,
-                                      decoration: BoxDecoration(
-                                          image: DecorationImage(
-                                            image: NetworkImage(
-                                                'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png'),
-                                          ),
-                                          color: Style.whiteColor,
-                                          borderRadius:
-                                              BorderRadius.circular(12)),
-                                    ),
-                                    16.horizontalSpace,
-                                    Expanded(
-                                      child: Container(
-                                        child: Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              'Software Engineer',
-                                              overflow: TextOverflow.ellipsis,
-                                              maxLines: 1,
-                                              style: Style.textStyleRegular2(
-                                                  textColor: Style.whiteColor),
-                                            ),
-                                            Text(
-                                              'Facebook',
-                                              style: Style.textStyleRegular2(
-                                                  size: 14,
-                                                  textColor: Style.whiteColor),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 12, top: 24, right: 12),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Wrap(
-                                        children: [
-                                          Container(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: 16, vertical: 4),
-                                            decoration: BoxDecoration(
-                                                color: Style.whiteColor
-                                                    .withOpacity(0.15),
-                                                borderRadius:
-                                                    BorderRadius.circular(65)),
-                                            child: Text(
-                                              'IT',
-                                              style: Style.textStyleRegular2(
-                                                  size: 12,
-                                                  textColor: Style.whiteColor),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      Wrap(
-                                        children: [
-                                          Container(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: 16, vertical: 4),
-                                            decoration: BoxDecoration(
-                                                color: Style.whiteColor
-                                                    .withOpacity(0.15),
-                                                borderRadius:
-                                                    BorderRadius.circular(65)),
-                                            child: Text(
-                                              'Full-Time',
-                                              style: Style.textStyleRegular2(
-                                                  size: 12,
-                                                  textColor: Style.whiteColor),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      Wrap(
-                                        children: [
-                                          Container(
-                                            padding: EdgeInsets.symmetric(
-                                                horizontal: 16, vertical: 4),
-                                            decoration: BoxDecoration(
-                                                color: Style.whiteColor
-                                                    .withOpacity(0.15),
-                                                borderRadius:
-                                                    BorderRadius.circular(65)),
-                                            child: Text(
-                                              'Junior',
-                                              style: Style.textStyleRegular2(
-                                                  size: 12,
-                                                  textColor: Style.whiteColor),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                24.verticalSpace,
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 24),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        '\$180,00/year',
-                                        style: Style.textStyleRegular2(
-                                            size: 13,
-                                            textColor: Style.whiteColor),
-                                      ),
-                                      Text(
-                                        'California, USA',
-                                        style: Style.textStyleRegular2(
-                                            size: 13,
-                                            textColor: Style.whiteColor),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ));
+                      return FeaturedJobs(
+                        index: index,
+                        imageLogoCompany:
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png',
+                        CompanyName: 'Facebook',
+                        salary: '\$180,00/year',
+                        textPosition: 'Software Engineer',
+                        textlocation: 'California, USA',
+                        wrap1: 'IT',
+                        wrap2: 'Full-Time',
+                        wrap3: 'Junior',
+                      );
                     },
                   ),
                 ),
